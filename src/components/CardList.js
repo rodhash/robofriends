fdec5c8c21ab4a4fa43c4c4faf5ca6ceb8e25340
Rodrigo Hashimoto <rodhash@gmail.com>
2022-01-22 16:13:10 -0300

    Moving the project to github

---
+++ src/components/CardList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, idx) => {
+                    return (
+                        <Card key={user.id}
+                        id={robots[idx].id}
+                        name={robots[idx].name}
+                        email={robots[idx].email} />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CardList
+
